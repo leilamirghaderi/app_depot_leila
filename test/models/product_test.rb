6f7ab3566d7f8c481e4c8701c0655b8bcf648002
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-02-17 11:31:16 -0600

    Test fixtures and product models
    $ bin/rails test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,8 +1,34 @@
-test "product attributes must not be empty" do
-  product = Product.new
+class ProductTest < ActiveSupport::TestCase
+fixtures :products
+test "product price must be positive" do
+  product = Product.new(title: "My Book Title",
+                        description: "yyy",
+                        image_url: "zzz.jpg")
+  product.price = -1
   assert product.invalid?
-  assert product.errors[:title].any?
-  assert product.errors[:description].any?
-  assert product.errors[:price].any?
-  assert product.errors[:image_url].any?
+  assert_equal ["must be greater than equal to 0.01"],
+    product.errors[:price]
+
+  product.price = 1
+  assert product.valid?
+end
+
+def new_product(image_url)
+  Product.new(title: "My Book Title",
+              description: "yyy",
+              price: 1,
+              image_url: image_url)
+end
+test "image_url" do
+  ok = %w{ fred.gif fred.jpg fred.png FRED,jpg FRED.jpg  http://a.b.c/x/y/z/fred.gif}
+  bad = %w{ fred.doc fred.gif/more fred.gif.more}
+  ok.each do |image_url|
+    assert new_product (image_url).valid?,
+           "#{image_url} shouldn't be invalid"
+end
+bad.each do |image_url|
+    assert new_product (image_url).invalid?,
+     "#{image_url} shouldn't be valid"
+   end
+end
 end
