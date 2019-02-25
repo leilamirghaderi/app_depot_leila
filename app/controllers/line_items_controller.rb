281d999b179dd12c3d47594a7816c109b1cb5405
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-02-25 14:23:00 -0600

    Modify and moving the Cart
    $ bin/rails test

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,17 +30,16 @@ end
     @line_item = @cart.add_product(product)
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
-          format.json { render :show,
-            status: :created, location: @line_item }
-          else
-            format.html { render :new }
-            format.json { render json: @line_item.errors,
-              status: :unprocessable_entity }
-            end
+        format.html { redirect_to store_index_url }
+        format.json { render :show,
+          status: :created, location: @line_item }
+        else
+          format.html { render :new }
+          format.json { render json: @line_item.errors,
+            status: :unprocessable_entity }
           end
         end
-
+      end
   # PATCH/PUT /line_items/1
   # PATCH/PUT /line_items/1.json
   def update
