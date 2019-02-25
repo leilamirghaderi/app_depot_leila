cc22875c414c44cacff274408dda19a4ce09a962
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-02-24 21:31:34 -0600

    Generating and migrating cart
    $ bin/rails generate scaffold Cart
    $ bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_18_155708) do
+ActiveRecord::Schema.define(version: 2019_02_25_005423) do
 
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
+    t.string "email"
+    t.integer "pay_time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
