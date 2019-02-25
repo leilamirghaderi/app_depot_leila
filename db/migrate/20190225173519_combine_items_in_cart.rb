58fb9cd7c8aaad680fffd627ba34a14a884b19e7
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-02-25 12:20:33 -0600

    Roll back the migration
    $ bin/rails db:rollback
    $ bin/rails db:migrate:status

--- db/migrate/20190225173519_combine_items_in_cart.rb
+++ db/migrate/20190225173519_combine_items_in_cart.rb
@@ -13,3 +13,15 @@ class CombineItemsInCart < ActiveRecord::Migration[5.2]
     end
   end
 end
+def down
+  LineItem.where("quantity>1").each do |line_item|
+    line_item.quantity.times do
+      LineItem.create(
+        cart_id: line_item.cart_id,
+        product_id: line_item.product_id,
+        quantity: 1
+      )
+    end
+    line_item.destroy
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_173519) do
+ActiveRecord::Schema.define(version: 2019_02_25_172328) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
