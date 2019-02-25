f6ab3824c13202dc5285a0a84d41f97bfba374bb
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-02-25 11:32:52 -0600

    Running the migration & add add_product() method in our cart & modify the line item controller & change to the show view
    $ bin/rails db:migrate

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,18 +27,20 @@ end
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
-
+    @line_item = @cart.add_product(product)
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
-        format.json { render :show, status: :created, location: @line_item }
-      else
-        format.html { render :new }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+        format.html { redirect_to @line_item.cart,
+          notice: 'Line item was successfully created.' }
+          format.json { render :show,
+            status: :created, location: @line_item }
+          else
+            format.html { render :new }
+            format.json { render json: @line_item.errors,
+              status: :unprocessable_entity }
+            end
+          end
+        end
 
   # PATCH/PUT /line_items/1
   # PATCH/PUT /line_items/1.json
