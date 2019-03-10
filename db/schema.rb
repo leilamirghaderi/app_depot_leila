14994aba1dcd4de66cb8c4cae349392efbd56163
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-03-10 18:34:59 -0500

    Migrating database $ rake db:drop db:create db:migrate $ rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_005423) do
+ActiveRecord::Schema.define(version: 2019_02_25_214943) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "discounts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "cart_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
+    t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["product_id"], name: "index_line_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
