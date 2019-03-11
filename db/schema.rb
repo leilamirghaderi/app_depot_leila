585b9b21e0a3a652cb49baee0ca334e292ceb85b
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-03-10 19:27:57 -0500

    Capturing an Order chapter 12
      $ bin/rails generate migration add_order_to_line_item order:references
      $ bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_214943) do
+ActiveRecord::Schema.define(version: 2019_03_11_002657) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -28,7 +28,9 @@ ActiveRecord::Schema.define(version: 2019_02_25_214943) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "quantity", default: 1
+    t.integer "order_id"
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["order_id"], name: "index_line_items_on_order_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
 
