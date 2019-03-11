cade3e11acdaf36ef0c38478c03724bf54f4c76d
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-03-11 12:35:56 -0500

    Configuring webpacker and installing react
    $ bundle install
    $ bin/rails webpacker:install
    $ rails webpacker:install
    $ bin/rails webpacker:install:react

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,10 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'book/action1'
-    get 'book/action2'
-  end
+  resources :orders
   resources :line_items
   resources :carts
-  #get 'store/index'
   root 'store#index', as: 'store_index'
-
-  resources :products, shallow: true do
-    resources :reviews
+  resources :products do
+    get :who_bought, on: :member
   end
-  resources :orders 
+
 end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
