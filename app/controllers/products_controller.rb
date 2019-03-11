1b347b5acc21a8557c3767aa8e21ee448faa7a99
Leila Mirghaderi <leila.mirghaderi@gmail.com>
2019-03-10 19:04:59 -0500

    changing controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -67,7 +67,7 @@ class ProductsController < ApplicationController
     end
   end
 
-  private 
+  private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
       @product = Product.find(params[:id])
